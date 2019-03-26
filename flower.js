cb0317ddd4922ca961453a2bc08b86adadbfd6a5
jwgoedert <jwgoedert@gmail.com>
2019-03-25 22:25:32 -0500

    Complete loading images and displaying array of shapes`

---
+++ flower.js
@@ -0,0 +1,32 @@
+const flowers = [
+  {
+    "name": "Flower1",
+    "latin": "flower1io",
+    "light": "sun", 
+    "ease": 1 
+  },
+  {
+    "name": "Flower2",
+    "latin": "flower2io",
+    "light": "part sun", 
+    "ease": 4 
+  },
+  {
+    "name": "Flower3",
+    "latin": "flower3io",
+    "light": "part sun", 
+    "ease": 2 
+  },
+  {
+    "name": "Flower4",
+    "latin": "flower4io",
+    "light": "shade", 
+    "ease": 2 
+  },
+  {
+    "name": "Flower5",
+    "latin": "flower5io",
+    "light": "shade", 
+    "ease": 3 
+  },
+]
